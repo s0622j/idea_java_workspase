2e55c756efb1a55534ea55122cdfc70f68e9a215
沈杰 <598759654@qq.com>
2022-06-20 08:29:30 +0800

    20220620

--- mybatis-demo-01/src/test/java/com/heima/test01/MyBatisTest.java
+++ mybatis-demo-01/src/test/java/com/heima/test01/MyBatisTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author shenjie
@@ -39,4 +41,126 @@ public class MyBatisTest {
         //5.释放资源
         sqlSession.close();
     }
+
+    @Test
+    public void testSelestById() throws IOException {
+        //接收参数
+        int id = 1;
+
+        //1.获取sqlsessionFactory对象
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3.获取Mapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        //4.执行方法
+        Brand brand = brandMapper.selectById(id);
+
+        System.out.println(brand);
+
+        //5.释放资源
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void testSelectByCondition() throws IOException {
+        //接收参数
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        // 处理参数
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        //封装对象
+        /*
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);
+        */
+
+        Map map = new HashMap();
+        map.put("status" , status);
+        map.put("companyName", companyName);
+        map.put("brandName" , brandName);
+
+        //1. 获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3. 获取Mapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        //4. 执行方法
+
+        //List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
+        //List<Brand> brands = brandMapper.selectByCondition(brand);
+        List<Brand> brands = brandMapper.selectByCondition(map);
+        System.out.println(brands);
+
+        //5. 释放资源
+        sqlSession.close();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+/**
+ *
+ * -- 删除tb_brand表
+ * drop table if exists tb_brand;
+ * -- 创建tb_brand表
+ * create table tb_brand
+ * (
+ *     -- id 主键
+ *     id           int primary key auto_increment,
+ *     -- 品牌名称
+ *     brand_name   varchar(20),
+ *     -- 企业名称
+ *     company_name varchar(20),
+ *     -- 排序字段
+ *     ordered      int,
+ *     -- 描述信息
+ *     description  varchar(100),
+ *     -- 状态：0：禁用  1：启用
+ *     status       int
+ * );
+ * -- 添加数据
+ * insert into tb_brand (brand_name, company_name, ordered, description, status)
+ * values ('三只松鼠', '三只松鼠股份有限公司', 5, '好吃不上火', 0),
+ *        ('华为', '华为技术有限公司', 100, '华为致力于把数字世界带入每个人、每个家庭、每个组织，构建万物互联的智能世界', 1),
+ *        ('小米', '小米科技有限公司', 50, 'are you ok', 1);
+ *
+ *
+ * SELECT * FROM tb_brand;
+ *
+ */
